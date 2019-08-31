bfd577b0de2dc42eabcd79d7c8bb2b54d6aa0e18
Steven Nguyen <steven.nguyen89@gmail.com>
2019-08-31 08:51:44 -0400

    Update scraping test case to load test data and test for expected output

--- market_data/test_scraper.py
+++ market_data/test_scraper.py
@@ -8,6 +8,10 @@ import datetime
 from scraper import Scraper, EquityData
 from scraper import InvalidSourceError, InvalidTickerError
 
+import os
+import inspect
+os.chdir((os.path.dirname(inspect.getfile(inspect.currentframe()))))
+
 class ScraperTests(unittest.TestCase):
 
     def test_scrape_from_valid_source(self):
@@ -21,17 +25,34 @@ class ScraperTests(unittest.TestCase):
 
 class ScraperYahooEquityPricesTests(unittest.TestCase):
 
+    def load_test_data(self):
+        test_file = 'amzn_scrape_test_data.html'
+        with open(test_file, 'rb') as f:
+            data = f.read()
+
+        return data
+
     @patch('urllib.request.urlopen', autospec=True)
-    def test_scrape_returns_data_object(self, mock_urlopen):
+    def test_scrape_returns_correct_equity_data(self, mock_urlopen):
         ticker = 'AMZN'
         dt = datetime.datetime(2019, 8, 23)
         scraper = Scraper('yahoo')
 
-        mock_urlopen.return_value.__enter__.return_value.status = 200
+        mock_urlopen_context = mock_urlopen.return_value.__enter__
+        mock_urlopen_context.return_value.status = 200
+        mock_urlopen_context.read.return_value = self.load_test_data()
 
         results = scraper.scrape_equity_data(ticker, dt)
+        expected_output = EquityData()
+        expected_output.open = 1793.03
+        expected_output.high = 1804.90
+        expected_output.low = 1745.23
+        expected_output.close = 1749.62
+        expected_output.adj_close = 1749.62
+        expected_output.volume = 5270800
 
         self.assertIsInstance(results, EquityData)
+        self.assertEqual(results, expected_output)
 
     @patch('urllib.request.urlopen', autospec=True)
     def test_scrape_invalid_ticker(self, mock_urlopen):
