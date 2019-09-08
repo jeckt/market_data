c425b9b1729f84d0729bc1aff94bfe91c0072458
Steven Nguyen <steven.nguyen89@gmail.com>
2019-09-08 10:56:17 -0400

    Add connect method and tests into DataAdapter class

--- market_data/data_adapter.py
+++ market_data/data_adapter.py
@@ -1,18 +1,20 @@
 import os
 import json
 
+# TODO(steve): should we turn the DataAdapter into a 
+# abstract class to provide an interface for the rest
+# of the app??
 class DataAdapter:
 
     test_database = 'testdb.txt'
+    prod_database = 'productiondb.txt'
 
     @classmethod
     def create_test_database(cls):
         if os.path.isfile(cls.test_database):
             raise DatabaseExistsError(cls.test_database)
 
-        with open(cls.test_database, 'w') as db:
-            import json
-            json.dump(list(), db)
+        cls._create_database(cls.test_database)
 
     @classmethod
     def delete_test_database(cls):
@@ -20,6 +22,33 @@ class DataAdapter:
             raise DatabaseNotFoundError(cls.test_database)
 
         os.remove(cls.test_database)
+
+    @classmethod
+    def _create_database(cls, database):
+        with open(database, 'w') as db:
+            import json
+            json.dump(list(), db)
+
+    @classmethod
+    def connect(cls, conn_string=None):
+        if conn_string is None:
+            conn_string = cls.prod_database
+            if not os.path.isfile(conn_string):
+                cls._create_database(conn_string)
+
+        if not os.path.isfile(conn_string):
+            raise DatabaseNotFoundError(conn_string)
+
+        return cls(conn_string)
+
+    def __init__(self, conn_string):
+        self.conn_string = conn_string
+
+    # NOTE(steve): this method will close the connection
+    # to the database. For the json implementation
+    # nothing needs to be done here.
+    def close(self):
+        pass
 
 class DatabaseExistsError(Exception):
     pass
--- market_data/tests/test_data_adapter.py
+++ market_data/tests/test_data_adapter.py
@@ -44,5 +44,29 @@ class DataAdapterTestDatabaseTests(unittest.TestCase):
         with self.assertRaises(DatabaseNotFoundError):
             DataAdapter.delete_test_database()
 
+    def test_connect_to_test_database(self):
+        DataAdapter.create_test_database()
+        self.assertTrue(os.path.isfile(DataAdapter.test_database))
+        da = DataAdapter.connect(DataAdapter.test_database)
+        self.assertEqual(da.conn_string, DataAdapter.test_database)
+
+class DataAdapterTests(unittest.TestCase):
+
+    def tearDown(self):
+        if os.path.isfile(DataAdapter.prod_database):
+            os.remove(DataAdapter.prod_database)
+
+    def test_create_and_connect_to_prod_database_by_default(self):
+        self.assertFalse(os.path.isfile(DataAdapter.prod_database))
+        da = DataAdapter.connect()
+        self.assertTrue(os.path.isfile(DataAdapter.prod_database))
+        self.assertEqual(da.conn_string, DataAdapter.prod_database)
+        da.close()
+
+    def test_connect_to_non_existing_database_throws_error(self):
+        self.assertFalse(os.path.isfile('newdb.txt'))
+        with self.assertRaises(DatabaseNotFoundError):
+            DataAdapter.connect('newdb.txt')
+
 if __name__ == '__main__':
     unittest.main()
