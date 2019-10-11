62e8e6687ee4b760f88555a0b0f421347c768ddd
Steven Nguyen <steven.nguyen89@gmail.com>
2019-10-11 18:04:08 -0400

    Typo in data adapter unit tests

--- market_data/tests/test_data_adapter.py
+++ market_data/tests/test_data_adapter.py
@@ -30,14 +30,14 @@ class DataAdapterSourceTests(unittest.TestCase):
         da = data_adapter.get_adapter(data_adapter.DataAdapterSource.SQLITE3)
         self.assertTrue(da, market_data.sqlite3_data_adapter.Sqlite3DataAdapter)
 
-@parameterized_class(('data_adapater_source', ),[
+@parameterized_class(('data_adapter_source', ),[
     [data_adapter.DataAdapterSource.JSON, ],
     [data_adapter.DataAdapterSource.SQLITE3, ]
 ])
 class DataAdapterTests(unittest.TestCase):
 
     def setUp(self):
-        self.da = data_adapter.get_adapter(self.data_adapater_source)
+        self.da = data_adapter.get_adapter(self.data_adapter_source)
 
     def tearDown(self):
         # NOTE(steve): I think it is acceptable to suppress
@@ -81,14 +81,14 @@ class DataAdapterTests(unittest.TestCase):
         with self.assertRaises(data_adapter.DatabaseNotFoundError):
             self.da.connect('newdb.json')
 
-@parameterized_class(('data_adapater_source', ),[
+@parameterized_class(('data_adapter_source', ),[
     [data_adapter.DataAdapterSource.JSON, ],
     [data_adapter.DataAdapterSource.SQLITE3, ]
 ])
 class DataAdapterSecuritiesTests(unittest.TestCase):
 
     def setUp(self):
-        self.da = data_adapter.get_adapter(self.data_adapater_source)
+        self.da = data_adapter.get_adapter(self.data_adapter_source)
         self.da.create_test_database()
         self.database = self.da.connect(self.da.test_database)
 
