dd6bbeb04c5230afe3aea745588e670fe2694acb
jeckt <steven.nguyen89@gmail.com>
2019-10-29 13:10:46 -0400

    Add doc strings to all methods in MarketData class

--- market_data/market_data.py
+++ market_data/market_data.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import json
+
 from market_data.scraper import Scraper
 from market_data.data import InvalidTickerError, InvalidDateError, NoDataError
 from market_data.data_adapter import DatabaseNotFoundError
@@ -20,6 +21,12 @@ class MarketData:
     # NOTE(steve): database param is a named tuple
     # Database = namedtuple('Database', ['conn_string', 'source'])
     def run(self, database):
+        """
+        Initialises MarketData class with scraper and data adapter.
+
+        Args:
+            database: namedtuple('Database', ['conn_string', 'source']).
+        """
         self.init = True
         self._scraper = Scraper('yahoo')
         da = data_adapter.get_adapter(database.source)
@@ -29,33 +36,90 @@ class MarketData:
     # all the dependency e.g. closing of the database
     # after the app is closed
     def close(self):
+        """Ensures dependent objects are properly cleaned up."""
         self.init = False
         self._database.close()
 
     # TODO(steve): should turn this into a decorator
     def _check_initialised(self):
+        """Checks if class is initialised."""
         if not self.init:
             raise NotInitialisedError('Call run method first!')
 
     def add_security(self, ticker):
+        """
+        Add security into market data.
+
+        Args:
+            ticker: Yahoo ticker for the security to be added.
+        """
         self._check_initialised()
         self._database.insert_securities([ticker])
 
     def get_securities_list(self):
+        """
+        Returns list of securities currently stored.
+
+        Returns:
+            List of security tickers.
+        """
         self._check_initialised()
         return self._database.get_securities_list()
 
     def get_equity_data(self, ticker, dt):
+        """
+        Returns equity data object (open, high, low, close, adj_close, volume)
+        for a selected ticker (Yahoo) and date.
+
+        Args:
+            ticker: Yahoo ticker.
+            dt: date of equity data.
+
+        Returns:
+            Equity data object (open, high, low, close, adj_close, volume)
+            for the selected ticker and date if available.
+
+        Raises:
+            InvalidTickerError: Security not in market data.
+            InvalidDateError: No equity data for date provided.
+        """
         self._check_initialised()
         data = self._database.get_equity_data(ticker, dt)
         return data
 
     def get_equity_data_series(self, ticker):
+        """
+        Return equity data for all available dates for the selected ticker.
+
+        Args:
+            ticker: Yahoo ticker.
+
+        Returns:
+            Equity data series sorted by date (newest to oldest) as a list of
+            tuples (date, equity_data).
+
+        Raises:
+            InvalidTickerError: Security not in market data.
+        """
         self._check_initialised()
         data = self._database.get_equity_data_series(ticker)
         return data
 
     def get_latest_equity_data(self, ticker):
+        """
+        Return the most recent equity data object for the selected ticker.
+
+        Args:
+            ticker: Yahoo ticker.
+
+        Returns:
+            Most recent equity data object by date (open, high, low, close,
+            adj_close, volume) for the selected ticker.
+
+        Raises:
+            InvalidTickerError: Security not in market data.
+            NoDataError: No data availabe for selected security.
+        """
         self._check_initialised()
         data = self.get_equity_data_series(ticker)
         if len(data) > 0:
@@ -64,6 +128,16 @@ class MarketData:
             raise NoDataError(ticker)
 
     def update_market_data(self, ticker, dt):
+        """
+        Updates market data for the selected security and date.
+
+        Args:
+            ticker: Yahoo ticker.
+            dt: date of equity data.
+
+        Raise:
+            InvalidTickerError: Security not in market data.
+        """
         self._check_initialised()
         if ticker in self._database.get_securities_list():
             data = self._scraper.scrape_equity_data(ticker, dt)
