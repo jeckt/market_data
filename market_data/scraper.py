952f2f0de321eb6b210bd787ce0450f5ea6b0ff7
Steven Nguyen <steven.nguyen89@gmail.com>
2019-12-13 19:32:24 -0500

    refactor scraper class

--- market_data/scraper.py
+++ market_data/scraper.py
@@ -12,19 +12,26 @@ class Scraper:
             raise InvalidSourceError(source)
         self.source = source
 
-    def scrape_equity_data(self, ticker, date):
-        # NOTE(steve): this allows the scrape equity data to accept
-        # both datetime and date objects. It only needs to the date.
+    # NOTE(steve): this allows the scrape equity data to accept
+    # both datetime and date objects. It only needs to the date.
+    @staticmethod
+    def _normalise_datetime(date):
         try:
-            date_only = date.date()
+            return date.date()
         except AttributeError:
-            date_only = date
+            return date
 
+    @staticmethod
+    def _get_web_page(ticker):
         url = r'https://finance.yahoo.com/quote/{ticker}/history?p={ticker}'
         req = urllib.request.Request(url.replace('{ticker}', ticker))
         with urllib.request.urlopen(req) as response:
             page = response.read().decode('utf-8')
 
+        return page
+
+    @staticmethod
+    def _get_hist_price_data_table(page, ticker):
         # NOTE(steve): parse the html page and find the table
         # of historical price data based on table attributes
         # to make it slightly more robust to changes in the webpage
@@ -37,45 +44,34 @@ class Scraper:
                                 attrs={'data-test':'historical-prices'})
             data_table = data_table.find('tbody')
 
-            for row in data_table.children:
-                values = [col.next_element.text for col in
-                          row.children if col.find('span')]
-
-                dt = datetime.datetime.strptime(values[0], '%b %d, %Y')
-                values[0] = dt.date()
-                if values[0] == date_only:
-                    d = EquityData(*(v.replace(',', '') for v in values[1:]))
-                    return d
+            return data_table
         except:
             raise InvalidTickerError(ticker)
 
-        raise InvalidDateError(f'{ticker}: {date}')
+    def scrape_equity_data(self, ticker, date):
+        date_only = Scraper._normalise_datetime(date)
+        page = Scraper._get_web_page(ticker)
+        data_table = Scraper._get_hist_price_data_table(page, ticker)
 
-    def _normalise_datetime(self, date):
-        try:
-            return date.date()
-        except AttributeError:
-            return date
+        for row in data_table.children:
+            values = [col.next_element.text for col in
+                      row.children if col.find('span')]
+
+            dt = datetime.datetime.strptime(values[0], '%b %d, %Y')
+            values[0] = dt.date()
+            if values[0] == date_only:
+                d = EquityData(*(v.replace(',', '') for v in values[1:]))
+                return d
+
+        raise InvalidDateError(f'{ticker}: {date}')
 
     def scrape_eq_multiple_dates(self, ticker, date_list):
         if date_list is None or len(date_list) == 0:
             raise EmptyDateListError(ticker)
 
-        clean_date_list = [self._normalise_datetime(dt) for dt in date_list]
-
-        url = r'https://finance.yahoo.com/quote/{ticker}/history?p={ticker}'
-        req = urllib.request.Request(url.replace('{ticker}', ticker))
-        with urllib.request.urlopen(req) as response:
-            page = response.read().decode('utf-8')
-
-        try:
-            parsed_html = BeautifulSoup(page, features='html.parser')
-            data_table = parsed_html.body.find('table',
-                                attrs={'data-test':'historical-prices'})
-            data_table = data_table.find('tbody')
-
-        except:
-            raise InvalidTickerError(ticker)
+        clean_date_list = [Scraper._normalise_datetime(dt) for dt in date_list]
+        page = Scraper._get_web_page(ticker)
+        data_table = Scraper._get_hist_price_data_table(page, ticker)
 
         data = {}
         for row in data_table.children:
